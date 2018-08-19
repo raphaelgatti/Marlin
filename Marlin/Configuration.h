cbea8ff52a9c4ce14639f579653f22a95607b696
Raphael Gatti <raphael-gatti-movile@movile.com>
2018-08-19 19:32:10 -0300

    Set zoffset from extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -771,7 +771,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 10  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 10  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.95   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.10   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
